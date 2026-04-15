84dc9bce4d6e7290df4ba7ce8d3b9a0f81dffee7
OMGUYISGAY111 <3321843528@qq.com>
2026-04-15 12:27:21 +0800

    breakchange

--- src/components/mainframe/homepage/homepage.vue
+++ src/components/mainframe/homepage/homepage.vue
@@ -1,33 +1,13 @@
-<script setup lang="ts">
-import Backtop from './backtop.vue';
-import Count from './count.vue';
-import PersonCard from './personCard.vue';
-
+<script setup>
 // 组件逻辑
 </script>
 
 <template>
-
-    <div></div>
-
-  <div class="grid">
-    <div class=" grid grid-cols-2 gap-2">
-      <PersonCard></PersonCard>
-      <PersonCard></PersonCard>
-      <PersonCard></PersonCard>
-      <PersonCard></PersonCard>
-      <PersonCard></PersonCard>
-    </div>
-
-
-    <p class=" text-4xl bg-blue-400">hello world</p>
-    <p class=" text-red-500">test</p>
-    <!-- <Card></Card> -->
-    <Count></Count>
-    <Backtop></Backtop>
+  <div>
+    
   </div>
 </template>
 
-<style scoped src="../homepage.css">
+<style scoped>
 
 </style>
