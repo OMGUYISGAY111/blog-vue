23e611dd9321a98b74542e36c601b1287142bff3
OMGUYISGAY111 <OMGUYISGAY111@users.noreply.github.com>
2026-04-27 06:28:47 +0000

    deploy: 8db65f21513496e9ebe63f7c617386e7628dce65

---
+++ assets/4-BX8Z9IMP.js
@@ -0,0 +1,7 @@
+import{c as e}from"./MarkdownRender-DeUTUfMX.js";var t=e({default:()=>n}),n=`---
+title: 真正的hello world?2
+date: 2026-04-16
+tags: [Markdown, 编程, 写作]
+---
+
+testLOL`;export{t as n,n as t};
