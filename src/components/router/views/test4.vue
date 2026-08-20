61aa17c781cb8fb1f581eec1b019cdfeb3caf6f6
OMGUYISGAY111 <3321843528@qq.com>
2026-08-20 21:53:13 +0800

    gread comple

--- src/components/router/views/test4.vue
+++ src/components/router/views/test4.vue
@@ -617,9 +617,263 @@ function decodeString(s: string): string {
 };
 
 function dailyTemperatures(temperatures: number[]): number[] {
-    
+
+    const indexStack:number[] = [];
+    let resArray:number[] = new Array<number>(temperatures.length).fill(0);
+
+    for (let i = 0;i < temperatures.length;i++) {
+
+        while (indexStack.length !== 0 && temperatures[indexStack[indexStack.length - 1]] < temperatures[i]) {
+            const toCalInd = indexStack.pop();
+            if (toCalInd !== undefined) {
+                resArray[toCalInd] = i - toCalInd;
+            }
+        }
+
+        indexStack.push(i);
+    }
+
+    return resArray;
 };
 
+function largestRectangleArea2(heights: number[]): number {
+
+    const DealHeight = Array.from(heights);
+
+    function nearestSmallValue(values:number[]): number[] {
+
+        const indexStack:number[] = [];
+        let resArray:number[] = new Array<number>(values.length).fill(0);
+
+        for (let i = 0;i < values.length;i++) {
+
+            while (indexStack.length !== 0 && values[indexStack[indexStack.length - 1]] > values[i]) {
+                const toCalInd = indexStack.pop();
+                if (toCalInd !== undefined) {
+                    resArray[toCalInd] = i - toCalInd;
+                }
+            }
+
+            indexStack.push(i);
+        }
+
+        return resArray;
+
+    }
+
+    const dealDisFunc = (dis:number,ind:number) => {
+        if (dis === 0) {
+            return heights.length - ind - 1;
+        } else {
+            return dis - 1;
+        }
+    }
+
+    const dealDisFuncRTL = (dis:number,ind:number) => {
+        if (dis === 0) {
+            return ind;
+        } else {
+            return dis - 1;
+        }
+    }
+
+    const leftDisArrUndo = nearestSmallValue(heights);
+    const rightDisArrUndo = nearestSmallValue(DealHeight.reverse()).reverse();
+    const leftDisArr:number[] = leftDisArrUndo.map(dealDisFunc);
+
+    console.log(leftDisArr)
+    const rightDisArr:number[] = rightDisArrUndo.map(dealDisFuncRTL);
+    console.log(rightDisArr)
+
+    const widArray:number[] = new Array<number>(heights.length).fill(0);
+
+    for (let i = 0;i< leftDisArr.length;i++) {
+        let Wid = leftDisArr[i] + 1 + rightDisArr[i];
+        widArray[i] = Wid;
+    }
+
+    let maxArea = widArray[0] * heights[0];
+
+    for (let i = 1;i < heights.length;i++) {
+        maxArea = Math.max(maxArea, widArray[i] * heights[i]);
+    }
+
+    return maxArea
+
+};
+
+function nearestSmallValue(values:number[]): number[] {
+
+    const indexStack:number[] = [];
+    let resArray:number[] = new Array<number>(values.length).fill(0);
+
+    for (let i = 0;i < values.length;i++) {
+
+        while (indexStack.length !== 0 && values[indexStack[indexStack.length - 1]] > values[i]) {
+            const toCalInd = indexStack.pop();
+            if (toCalInd !== undefined) {
+                resArray[toCalInd] = i - toCalInd;
+            }
+        }
+
+        indexStack.push(i);
+    }
+
+    return resArray;
+
+}
+
+function largestRectangleArea(heights: number[]): number {
+
+    const indexStack:number[] = [0];
+    const DealHeight = [0, ...heights, 0]
+
+    let max = -Infinity
+
+    for (let i = 1;i < DealHeight.length;i++) {
+
+        while (indexStack.length > 1 && DealHeight[indexStack[indexStack.length - 1]] >= DealHeight[i]) {
+            const toCalInd = indexStack.pop() ?? 0;
+            const width = i - indexStack[indexStack.length - 1] - 1;
+            max = Math.max(max, width * DealHeight[toCalInd]);
+        }
+
+        indexStack.push(i);
+    }
+
+    return max;    
+}
+
+function maxProfit2(prices: number[]): number {
+    const fixPrice = [-Infinity,...prices,-Infinity];
+
+    let max = -Infinity;
+    let peak = prices[0],bottom = prices[0];
+
+    for (let i = 1; i < prices.length + 1;i++) {
+        if (fixPrice[i - 1] < fixPrice[i] && fixPrice[i + 1] < fixPrice[i]) {
+            
+            peak = Math.max(peak, fixPrice[i]);
+            max = Math.max(max, peak - bottom);
+
+        } else if (fixPrice[i - 1] > fixPrice[i] && fixPrice[i + 1] > fixPrice[i]) {
+            
+            if (fixPrice[i] < bottom) {
+                max = Math.max(max, peak - bottom);
+                bottom = fixPrice[i];
+                peak = -Infinity;
+            }
+            
+        }
+    }
+
+    return max === -Infinity ? 0 : max;
+};
+
+function maxProfit(prices: number[]): number { 
+    let max = 0
+    let leftMin = prices[0]
+
+    for (let i = 1;i < prices.length;i++) {
+        leftMin = Math.min(leftMin, prices[i - 1])
+        if (prices[i] > leftMin) {
+            max = Math.max(max, prices[i] - leftMin);
+        }
+    }
+
+    return max;
+}
+
+function canJump(nums: number[]): boolean {
+    let limit = 0;
+    for (let i = 0;i <= limit;i++) {
+        limit = Math.max(limit,i + nums[i]);
+        if (limit >= nums.length - 1) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+function jump(nums: number[]): number {
+
+    let canReach = nums[0];
+    let goodPos = 0;
+    let time = 0;
+
+
+    if (goodPos === nums.length - 1) {
+        return time;
+    }
+
+    while (canReach < nums.length - 1) {
+
+        let nextCanReach = canReach
+
+        for (let i = goodPos + 1;i <= canReach;i++) {
+            if (i + nums[i] > nextCanReach) {
+                goodPos = i;
+                nextCanReach = i + nums[i];
+            }
+        }
+
+        time++;
+        canReach = nextCanReach;
+    }
+    time++;
+
+    return time;
+};
+
+function partitionLabels(s: string): number[] {
+    let letterMap:Map<string,number> = new Map();
+
+    let count = 0;
+
+    let resArray:number[] = [];
+
+    for (let i = 0;i < s.length;i++) {
+        letterMap.set(s[i],i);
+    }
+
+    let board:number = letterMap.get(s[0])!;
+
+    for (let i = 0;i < s.length;i++) {
+        
+        count++;
+
+        board = Math.max(board, letterMap.get(s[i])!);
+        
+        if (i === board) {
+            resArray.push(count);
+            count = 0
+        }
+    }
+
+    return resArray;
+};
+
+  class TreeNode {
+      val: number
+      left: TreeNode | null
+      right: TreeNode | null
+      constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+          this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+          this.right = (right===undefined ? null : right)
+      }
+ }
+
+
+console.log(jump([2,3,1,1,4]))
+// console.log(maxProfit([3,3,3]))
+// console.log(largestRectangleArea([2,1,5,6,2,3]))
+// let another = [2,1,5,6,2,3].reverse()
+// console.log("left to right",nearestSmallValue([2,1,5,6,2,3]))
+// console.log("right to left",nearestSmallValue(another).reverse())
+
+// console.log(dailyTemperatures([73,74,75,71,69,72,76,73]))
 // console.log(decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef"))
 //console.log(orangesRotting([[2,2],[1,1],[0,0],[2,0]]))
 let wtf = [[1,0],[2,0],[2,1],[3,1],[3,2],[4,2],[4,3],[5,3],[5,4],[6,4],[6,5],[7,5],[7,6],[8,6],[8,7],[9,7],[9,8],[10,8],[10,9],[11,9],[11,10],[12,10],[12,11],[13,11],[13,12],[14,12],[14,13],[15,13],[15,14],[16,14],[16,15],[17,15],[17,16],[18,16],[18,17],[19,17],[19,18],[20,18],[20,19],[21,19],[21,20],[22,20],[22,21],[23,21],[23,22],[24,22],[24,23],[25,23],[25,24],[26,24],[26,25],[27,25],[27,26],[28,26],[28,27],[29,27],[29,28],[30,28],[30,29],[31,29],[31,30],[32,30],[32,31],[33,31],[33,32],[34,32],[34,33],[35,33],[35,34],[36,34],[36,35],[37,35],[37,36],[38,36],[38,37],[39,37],[39,38],[40,38],[40,39],[41,39],[41,40],[42,40],[42,41],[43,41],[43,42],[44,42],[44,43],[45,43],[45,44],[46,44],[46,45],[47,45],[47,46],[48,46],[48,47],[49,47],[49,48],[50,48],[50,49],[51,49],[51,50],[52,50],[52,51],[53,51],[53,52],[54,52],[54,53],[55,53],[55,54],[56,54],[56,55],[57,55],[57,56],[58,56],[58,57],[59,57],[59,58],[60,58],[60,59],[61,59],[61,60],[62,60],[62,61],[63,61],[63,62],[64,62],[64,63],[65,63],[65,64],[66,64],[66,65],[67,65],[67,66],[68,66],[68,67],[69,67],[69,68],[70,68],[70,69],[71,69],[71,70],[72,70],[72,71],[73,71],[73,72],[74,72],[74,73],[75,73],[75,74],[76,74],[76,75],[77,75],[77,76],[78,76],[78,77],[79,77],[79,78],[80,78],[80,79],[81,79],[81,80],[82,80],[82,81],[83,81],[83,82],[84,82],[84,83],[85,83],[85,84],[86,84],[86,85],[87,85],[87,86],[88,86],[88,87],[89,87],[89,88],[90,88],[90,89],[91,89],[91,90],[92,90],[92,91],[93,91],[93,92],[94,92],[94,93],[95,93],[95,94],[96,94],[96,95],[97,95],[97,96],[98,96],[98,97],[99,97]]
